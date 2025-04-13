07bed9afcfe86dfc2e2f7fab7ea4bf08f60a5126
Yanni Fraga <yanni_fernanda@hotmail.com>
2025-04-13 17:53:55 +0200

    Fixing auth folder structure and authjs dependecy

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl">Welcome to the Jungle</h1>
+      <h1 className="text-2xl">Home</h1>
     </>
   );
 }
--- auth.ts
+++ auth.ts
@@ -1,11 +1,6 @@
 import NextAuth from "next-auth";
-import GitHubProvider from "next-auth/providers/github";
+import GitHub from "next-auth/providers/github"
 
-export const { handlers, signIn, signOut, auth} = NextAuth({
-  providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? '',
-      clientSecret: process.env.GITHUB_SECRET ?? ''
-    })
-  ]
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub],
 })
