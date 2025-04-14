849e6715e09d2b45bb8d91c78497a5bdfaeb4e4a
Yanni Fraga <yanni_fernanda@hotmail.com>
2025-04-14 17:46:48 +0200

    Startup card component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,7 +1,19 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 
-export default async function Home({ searchParams }: { searchParams: Promise<{query?: string}> }) {
+export default async function Home({searchParams}: { searchParams: Promise<{ query?: string }> }) {
   const query = (await searchParams).query
+
+  const posts: StartupTypeCard[] = [{
+    _id: 1,
+    title: 'Cool Shop',
+    description: 'Cool description',
+    category: 'E-commerce',
+    image: 'https://images.unsplash.com/photo-1743001025958-19eca24afd23?q=80&w=1740&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    author: {_id: 1, name: 'Yanni Frufru'},
+    views: 101,
+    _createdAt: new Date().toString(),
+  }]
 
   return (
     <>
@@ -13,7 +25,22 @@ export default async function Home({ searchParams }: { searchParams: Promise<{qu
         <p className="sub-heading !max-w-3xl">
           Submit ideas, vote on pitches and get noticed in virtual competitions.
         </p>
-        <SearchForm query={query} />
+        <SearchForm query={query}/>
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : 'All Startups'}
+        </p>
+        <div className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts?.map((post: StartupTypeCard) => (
+              <StartupCard key={post._id} post={post} />
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </div>
       </section>
     </>
   );
---
+++ components/StartupCard.tsx
@@ -0,0 +1,86 @@
+import {EyeIcon, User} from "lucide-react";
+import Link from "next/link";
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {formatDate} from "@/lib/utils";
+
+export type StartupTypeCard = {
+  _id: number,
+  title: string,
+  description: string,
+  category: string,
+  image: string,
+  author: { _id: number, name: string, image?: string },
+  views: number,
+  _createdAt: string,
+}
+
+const StartupCard = ({post}: { post: StartupTypeCard }) => {
+  const {
+    _createdAt,
+    views,
+    author,
+    title,
+    category,
+    _id,
+    image,
+    description,
+  } = post;
+
+  return (
+    <div className="startup-card group">
+      <div className="flex-between">
+        <p className="startup_card_date">{formatDate(_createdAt)}</p>
+        <div className="flex gap-1.5">
+          <EyeIcon className="size-6 text-primary"/>
+          <span className="text-16-medium">{views}</span>
+        </div>
+      </div>
+
+      <div className="flex-between gap-5 mt-5">
+        <div className="flex-1">
+          <Link href={`/user/${author?._id}`}>
+            <p className="text-16-medium line-clamp-1">{author?.name}</p>
+          </Link>
+          <Link href={`/startup/${_id}`}>
+            <h3 className="text-26-semibold line-clamp-1">{title}</h3>
+          </Link>
+        </div>
+        <Link href={`/user/${author?._id}`}>
+          {author?.image ? (
+            <Image
+              src={author?.image}
+              alt={author?.name}
+              width={48}
+              height={48}
+              className="rounded-full"
+            />
+            )
+            : (
+              <div className="flex items-center justify-center size-12 bg-primary rounded-full">
+                <User className="size-8 text-white"/>
+              </div>
+            )
+          }
+        </Link>
+      </div>
+
+      <Link href={`/startup/${_id}`}>
+        <p className="startup-card_desc">{description}</p>
+
+        <Image src={image} alt="placeholder" className="startup-card_img"/>
+      </Link>
+
+      <div className="flex-between gap-3 mt-5">
+        <Link href={`/?query=${category?.toLowerCase()}`}>
+          <p className="text-16-medium">{category}</p>
+        </Link>
+        <Button className="startup-card_btn" asChild>
+          <Link href={`/startup/${_id}`}>Details</Link>
+        </Button>
+      </div>
+    </div>
+  )
+}
+
+export default StartupCard;
