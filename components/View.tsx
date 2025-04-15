e2ea01aadfea2af7e0b7d41d40fd16f51629cc52
Yanni Fraga <yanni_fernanda@hotmail.com>
2025-04-15 03:04:45 +0200

    Views components and updating view count on visit

--- components/View.tsx
+++ components/View.tsx
@@ -1,14 +1,23 @@
+import {after} from 'next/server'
 import Ping from "@/components/Ping";
 import {client} from "@/sanity/lib/client";
 import {STARTUP_VIEWS_QUERY} from "@/sanity/lib/queries";
+import {writeClient} from "@/sanity/lib/write-client";
 
-const View = async ({ id }: { id: string }) => {
-  const totalViews = await client.withConfig({ useCdn: false }).fetch(STARTUP_VIEWS_QUERY, { id })
+const View = async ({id}: { id: string }) => {
+  const totalViews = await client.withConfig({useCdn: false}).fetch(STARTUP_VIEWS_QUERY, {id})
+
+  after(async () =>
+    await writeClient
+      .patch(id)
+      .set({ views: totalViews.views + 1 })
+      .commit()
+  )
 
   return (
     <div className="view-container">
       <div className="absolute -top-2 -left-1">
-        <Ping />
+        <Ping/>
       </div>
       <p className="view-text">
         <span className="font-black">Views: {totalViews.views}</span>
