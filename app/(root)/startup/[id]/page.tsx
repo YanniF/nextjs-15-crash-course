81fcad541625b7eeff45bf47eba02bfd5b2ded25
Yanni Fraga <yanni_fernanda@hotmail.com>
2025-04-17 03:03:16 +0200

    Editor pick's query

--- app/(root)/startup/[id]/page.tsx
+++ app/(root)/startup/[id]/page.tsx
@@ -5,9 +5,10 @@ import {notFound} from "next/navigation";
 import markdownit from "markdown-it";
 import {formatDate} from "@/lib/utils";
 import {client} from "@/sanity/lib/client";
-import {STARTUP_BY_ID_QUERY} from "@/sanity/lib/queries";
+import {PLAYLIST_BY_SLUG_QUERY, STARTUP_BY_ID_QUERY} from "@/sanity/lib/queries";
 import {Skeleton} from "@/components/ui/skeleton";
 import View from "@/components/View";
+import StartupCard, {StartupTypeCard} from "@/components/StartupCard";
 
 const md = markdownit()
 
@@ -15,7 +16,14 @@ const md = markdownit()
 const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
   const id = (await params).id;
 
-  const post = await client.fetch(STARTUP_BY_ID_QUERY, { id })
+  const [post, playlistPosts] = await Promise.all([
+    client.fetch(STARTUP_BY_ID_QUERY, { id }),
+    client.fetch(PLAYLIST_BY_SLUG_QUERY, {
+      slug: 'yannis-picks-01',
+    }),
+  ]);
+
+  const editorPosts = playlistPosts?.select ?? []
 
   if(!post) {
     return notFound()
@@ -76,17 +84,17 @@ const Page = async ({ params }: { params: Promise<{ id: string }> }) => {
 
         <hr className="divider" />
 
-        {/*{editorPosts?.length > 0 && (*/}
-        {/*  <div className="max-w-4xl mx-auto">*/}
-        {/*    <p className="text-30-semibold">Editor Picks</p>*/}
+        {editorPosts?.length > 0 && (
+          <div className="max-w-4xl mx-auto">
+            <p className="text-30-semibold">Editor Picks</p>
 
-        {/*    <ul className="mt-7 card_grid-sm">*/}
-        {/*      {editorPosts.map((post: StartupTypeCard, i: number) => (*/}
-        {/*        <StartupCard key={i} post={post} />*/}
-        {/*      ))}*/}
-        {/*    </ul>*/}
-        {/*  </div>*/}
-        {/*)}*/}
+            <div className="mt-7 card_grid-sm">
+              {editorPosts.map((post: StartupTypeCard, i: number) => (
+                <StartupCard key={i} startup={post} />
+              ))}
+            </div>
+          </div>
+        )}
 
         {/* PPR */}
         <Suspense fallback={<Skeleton className="view_skeleton" />}>
